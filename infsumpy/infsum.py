e0b8f971d09da35cadef3ac9567fba31965a6698
Wellington36 <wellington.71319@gmail.com>
2024-11-17 09:10:37 -0300

    Add setup

--- infsumpy/infsum.py
+++ infsumpy/infsum.py
@@ -8,11 +8,14 @@ def infsum(f, method: str, M: int, initial: int, eps=mpf(2)**mpf(-52), L=mpf(0),
     mp.prec = precision
 
     if method == 'fixed':
-        return exp(FixedSum(f, M, initial))
+        r = FixedSum(f, M, initial)
+        return (r[0], float(exp(r[1])))
     elif method == 'threshold':
-        return exp(ThresholdSum(f, M, eps, initial))
+        r = ThresholdSum(f, M, eps, initial)
+        return (r[0], float(exp(r[1])))
     elif method == 'ratio':
-        return exp(RatioTestSum(f, M, L, eps, initial))
+        r = RatioTestSum(f, M, L, eps, initial)
+        return (r[0], float(exp(r[1])))
     else:
         raise ValueError(f"Method {method} not supported, try 'fixed'")
 
---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+VERSION = '1.0.0'
+DESCRIPTION = 'Approximate infinite sums with a guaranteed error.'
+
+# Setting up
+setup(
+    name="InfSumPy",
+    version=VERSION,
+    author="Wellington Silva",
+    author_email="<wellington.71319@gmail.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=open('README.md').read(),
+    url="https://github.com/wellington36/InfSumPy",
+    license='MIT',
+    packages=find_packages(),
+    install_requires=['mpmath'],
+    keywords=['python', 'sum', 'series', 'infinite-series', 'approximation', 'guaranted-sum']
+)
